7229e1e45a9f11c50fcb11d90e9b73ddcf873239
SNC Senpai aka Shaz <62912421+sncsenpai@users.noreply.github.com>
2025-09-19 11:37:25 +0100

    Update glideDialogWindow.js

--- snippets/client-side/glideDialogWindow.js
+++ snippets/client-side/glideDialogWindow.js
@@ -1,15 +1,13 @@
-var tableName = g_form.getTableName();
-var sysID = g_form.getUniqueValue();
-	
-var gdw = new GlideDialogWindow('close_change', false, 400, 200); 
-gdw.setTitle('Close Change'); 
-gdw.setPreference('table', tableName); 
-gdw.setPreference('sysparm_view', 'close_change'); 
-gdw.setPreference('sysparm_form_only', 'true');
+var tableName = g_form.getTableName(); 
+var sysID = g_form.getUniqueValue(); 
 
-//Set the query to display the record 
-var query = 'sys_id=' + sysID; 
-gdw.setPreference('sysparm_query', query); 
+var gdw = new GlideDialogWindow('close_record'); 
+gdw.setTitle('Close Record'); 
+gdw.setSize(500,400); 
+gdw.setPreference('table', tableName); // Don't use _list so we get a form
+gdw.setPreference('sysparm_view', 'ess'); 
+gdw.setPreference('sysparm_form_only', 'true'); // don't show related lists
+gdw.setPreference('sys_id', sysID);
 
 //Open the dialog window 
 gdw.render(); 
