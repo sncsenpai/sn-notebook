9097d748231775b58ecfde4e77103af8041220d5
SNC Senpai aka Shaz <62912421+sncsenpai@users.noreply.github.com>
2025-09-19 17:22:13 +0100

    Update getTableExtensions.js

--- snippets/server-side/getTableExtensions.js
+++ snippets/server-side/getTableExtensions.js
@@ -2,3 +2,12 @@
 var parentTableName = "cmdb_ci_server";
 var aExtensions = GlideDBObjectManager.get().getTableExtensions(parentTableName);
 gs.print(aExtensions);
+
+// or 
+
+var tableName = "cmdb_ci_server",
+  jlist = GlideDBObjectManager.get().getAllExtensions(),
+  list = j2js(jlist);
+for (var c = list.length; c > 0;) {
+  gs.print(list[--c])
+} 
