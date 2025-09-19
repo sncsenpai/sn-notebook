93a96bc5a86f111ceee4dbc50023e6b9a178167c
SNC Senpai aka Shaz <62912421+sncsenpai@users.noreply.github.com>
2025-09-19 15:08:23 +0100

    Create xmlView.js

---
+++ snippets/client-side/xmlView.js
@@ -0,0 +1,5 @@
+/* 
+ * Run this in the console if you don't have the Show XML option when impersonating a user 
+ * Useful for troubleshooting. Or even to grab data from a related table.
+ */
+xmlView('table_name', 'sys_id');
